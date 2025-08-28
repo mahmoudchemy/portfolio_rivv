59f2e88cadf279b704fca2733f73d4a02a4346de
mahmoudchemy <realmahmoudcodes@gmail.com>
2025-08-28 02:12:03 +0100

    'implement all the changes made to the code'

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react'
 import { BentoGrid, BentoGridItem } from './ui/BentoGrid'
 import { gridItems } from '@/data'
@@ -6,12 +7,17 @@ const Grid = () => {
   return (
     <section id='about'>
         <BentoGrid>
-          {gridItems.map((item) =>(
+          {gridItems.map(({id,description,title,className,img,imgClassName,titleClassName,spareImg}) =>(
             <BentoGridItem 
-              id={item.id}
-              key={item.id}
-              title={item.title}
-              description={item.description}
+              id={id}
+              key={id}
+              title={title}
+              description={description}
+              className={className}
+              img={img}
+              imgClassName={imgClassName}
+              titleClassName={titleClassName}
+              spareImg={spareImg}
             />
           ))}
         </BentoGrid>
