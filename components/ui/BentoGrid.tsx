a6e8cb71b58a427e343c3f6384f63e6235a96964
mahmoudchemy <realmahmoudcodes@gmail.com>
2025-08-28 15:28:37 +0100

    'implement all the changes made to the code'

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -4,15 +4,15 @@ import { useState } from "react";
 import { IoCopyOutline } from "react-icons/io5";
 
 // Also install this npm i --save-dev @types/react-lottie
-// import Lottie from "react-lottie";
+import Lottie from "react-lottie";
 
 import { cn } from "@/lib/utils";
 
 
-// import GridGlobe from "./GridGlobe";
-// import animationData from "@/data/confetti.json";
-import MagicButton from "./MagicButton";
 import { BackgroundGradientAnimation } from "./GradientBg";
+import GridGlobe from "./GridGlobe";
+import animationData from "@/data/confetti.json";
+import MagicButton from "./MagicButton";
 
 export const BentoGrid = ({
   className,
@@ -59,14 +59,14 @@ export const BentoGridItem = ({
 
   const [copied, setCopied] = useState(false);
 
-  // const defaultOptions = {
-  //   loop: copied,
-  //   autoplay: copied,
-  //   // animationData: animationData,
-  //   rendererSettings: {
-  //     preserveAspectRatio: "xMidYMid slice",
-  //   },
-  // };
+  const defaultOptions = {
+    loop: copied,
+    autoplay: copied,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
 
   const handleCopy = () => {
     const text = "hsu@jsmastery.pro";
@@ -103,7 +103,7 @@ export const BentoGridItem = ({
         <div
           className={`absolute right-0 -bottom-5 ${id === 5 && "w-full opacity-80"
             } `}
-          >
+        >
           {spareImg && (
             <img
               src={spareImg}
@@ -139,7 +139,7 @@ export const BentoGridItem = ({
           </div>
 
           {/* for the github 3d globe */}
-          {/* {id === 2 && <GridGlobe />} */}
+          {id === 2 && <GridGlobe />}
 
           {/* Tech stack list div */}
           {id === 3 && (
@@ -182,7 +182,7 @@ export const BentoGridItem = ({
                   }`}
               >
                 {/* <img src="/confetti.gif" alt="confetti" /> */}
-                {/* <Lottie options={defaultOptions} height={200} width={400} /> */}
+                <Lottie options={defaultOptions} height={200} width={400} />
               </div>
 
               <MagicButton
