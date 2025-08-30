7e7714f1f8e02a94e12df50d480b66c77c34d9ba
mahmoudchemy <realmahmoudcodes@gmail.com>
2025-08-30 14:43:49 +0100

    'implement the testimonials section'

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Clients from "@/components/Clients";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
@@ -14,6 +15,7 @@ export default function Home() {
         <Hero />
         <Grid />
         <RecentProjects />
+        <Clients/>
       </div>
     </main>
   );
